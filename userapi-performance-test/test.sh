dbf40579f04fbb1c566e88e8281bf561eb0d6e0f
david-iaggbs <david.parra@iaggbs.com>
2019-03-22 09:55:43 +0000

    fix functional and performance testing

--- userapi-performance-test/test.sh
+++ userapi-performance-test/test.sh
@@ -3,5 +3,5 @@ set -e
 
 
 curl --request GET --url 'https://api.getpostman.com/environments/651996-b80ed237-9179-35ec-d8a1-35d574e118c8' --header 'X-Api-Key: 100822fe2bd7454eb916c8ebdd4be266' > ci.json
-wget -O collection.json https://www.getpostman.com/collections/6d9fe4f1a0033de5a2af
+wget -O collection.json https://www.getpostman.com/collections/f685e5d789063b1093f8
 bzt /bzt-configs/test.yml -o modules.blazemeter.report-name="$REPORT"
